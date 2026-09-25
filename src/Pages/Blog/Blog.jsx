e425d5693c44fdaa3cfd1952312b179b04fd977c
Ahmed Salama <ahmedsalama536hdg@gmail.com>
2026-09-26 00:03:38 +0300

    update

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -679,7 +679,7 @@ let updateFilterCategory = filterCategory.slice(0,6)
     <>
       <section
         id="?"
-        className="relative bg-black text-white  flex flex-col justify-center items-center   overflow-hidden pt-14 "
+        className="relative bg-black text-white  flex flex-col justify-center items-center   overflow-hidden pt-10 "
       >
         <div className="absolute top-32 left-32 w-[350px] h-[350px] bg-orange-400 rounded-full blur-[140px] opacity-26 pointer-events-none z-0"></div>
         <div className="absolute inset-0 bg-[linear-gradient(to_right,#1e293b_1px,transparent_1px),linear-gradient(to_bottom,#1e293b_1px,transparent_1px)] bg-[size:3rem_3rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_50%,#000_70%,transparent_100%)] opacity-40" />
